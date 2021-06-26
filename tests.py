eba3b9db8f5ae6e54f1571fb1791ed5ed70403da
mdauthentic <muideen.lawal20@gmail.com>
2021-06-26 08:26:05 +0200

    add tests

--- tests.py
+++ tests.py
@@ -15,4 +15,28 @@ def sample_data():
             consectetur adipiscing elit."""
 
 
+@pytest.mark.parametrize(
+    "test_data, expected",
+    [
+        ("00:00:12,815", "00:00:12:815"),
+        ("00:00:14,509", "00:00:14:509"),
+        ("00:00:16,934", "00:00:16:934"),
+    ],
+)
+def test_format_time_delta(srt, test_data, expected):
+    assert srt.format_time_delta(test_data) == expected
 
+
+def test_parse(sample_data):
+    return_data = [
+        {
+            "start_time": "00:00:12:815",
+            "end_time": "00:00:14:509",
+            "subtitle_text": "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        }
+    ]
+    assert Subtitle(srt_string=sample_data).parse() == return_data
+
+
+def test_file_size_conv(srt):
+    assert srt.size_in_bytes(2000) == "2.0 KB"
